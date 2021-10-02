e94dd3128dd7fc73eb9deb34a082e646ff6e691a
IanJames2 <ianjms81@gmail.com>
2021-10-02 14:20:10 -0500

    Next: Getting Input From The User

---
+++ Java_Fundamentals_FastPaced/Java-Fundamentals-Attempt/Lesson_01/src/PrintSimpleArithmeticOps.java
@@ -0,0 +1,6 @@
+public class PrintSimpleArithmeticOps {
+    public static void main(String args[]) {
+        System.out.println("The sum of " + 3 + "+" + 4 + " is " + 3 + 4);
+        System.out.println("The product of " + 3 + "*" + 4 + " is " + 3 * 4);
+    }
+}
